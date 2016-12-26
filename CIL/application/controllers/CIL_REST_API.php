830980fa0626ac60401bfc8dea8019919adbb195
Willy <wawong@gmail.com>
2016-12-25 17:10:45 -0800

    REST API UPDATE

--- CIL/application/controllers/CIL_REST_API.php
+++ CIL/application/controllers/CIL_REST_API.php
@@ -1,10 +1,56 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+spl_autoload_register(function($class){
+    $path = str_replace("\\", "/", $class);
+    //echo "<br/>Path---".$path;
+    
+    $folder = "C:/Users/Willy/Documents/php-lib/Elastica/lib/";
+    //$folder = '/var/www/Elastica/lib/';
+   if (file_exists($folder . $path . '.php')) {
+       
+       //echo '/var/www/Elastica/lib/' . $path . '.php -- Found!';
+        require_once($folder . $path . '.php');
+    }
+    else
+         echo $folder . $path . '.php -- NOT Found!';
+
+});
 
 //require_once 'C:\Users\Willy\Documents\apache\Apache24\html\CIL\application\libraries\REST_Controller.php';
 require_once './application/libraries/REST_Controller.php';
 
 class CIL_REST_API extends REST_Controller
 {
+    
+  public function user_key_auth_get($key=0)
+  {
+      require_once 'CILServiceUtil.php';
+      $sutil = new CILServiceUtil();
+      $result = $sutil->getCIL_user_key($key);
+      //var_dump($result[0]->getHit());
+      
+      
+      
+      $results = $result->getResults();
+      if(count($results) >0)
+      { 
+          //$item = $hits = $results[0];
+          //var_dump($item);
+          $mainA = array();
+          $mainA['valid_key'] =true;
+          $this->response($mainA);
+      }
+      else
+      {
+          $mainA = array();
+          $mainA['valid_key'] =false;
+          $this->response(false);
+      }
+          
+      //if(!is_null($result) && isset($result['_results']))
+      //    var_dump($result['_results']);
+      //$this->response('Testing...');
+  }
+    
   public function image_get($id=0,$type='ALL')
   {
     //$cwd =  getcwd();
