4d307d11271d0d30c691902e291758c0d32f364d
wawong <wawong@gmail.com>
2017-06-21 15:58:56 -0700

    Add wavelength

--- CIL/application/views/cil_inner_pages/cil_inner_dimension.php
+++ CIL/application/views/cil_inner_pages/cil_inner_dimension.php
@@ -2,8 +2,11 @@
     if(isset($json->CIL_CCDB->CIL->CORE->DIMENSION))
     {
         $hasImageSize = false;
+        $hasWavelength = false;
         
         $dimension = $json->CIL_CCDB->CIL->CORE->DIMENSION;
+        
+        //Checking whether image size exists
         foreach($dimension as $dim)
         {
             if(isset($dim->Image_size))
@@ -12,6 +15,17 @@
                 break;
             }
         }
+        
+        //Checking whether wavelength exists
+        foreach($dimension as $dim)
+        {
+            if(isset($dim->Space) && strcmp($dim->Space,"wavelength") == 0)
+            {
+                $hasWavelength = true;
+                break;
+            }
+        }
+        
         
 ?>
         <div class='biological_sources'>
@@ -52,7 +66,8 @@
                         if(isset($dim->Image_size))
                         {
                             echo $dim->Image_size;
-                            echo "<abbr title=\"pixels\">px</abbr>";
+                            //echo "<abbr title=\"pixels\">px</abbr>";
+                            echo "px";
                         }
                         else
                         {
@@ -69,9 +84,9 @@
                         if(isset($dim->Pixel_size->unit))
                         {
                             
-                            if(strcmp($dim->Pixel_size->unit, "nanometers"))
+                            if(strcmp($dim->Pixel_size->unit, "nanometers")==0)
                                 echo "<abbr title=\"nanometers\">nm</abbr></td>";
-                            else if(strcmp($dim->Pixel_size->unit, "microns"))
+                            else if(strcmp($dim->Pixel_size->unit, "microns")==0)
                                 echo "<abbr title=\"microns\">&micro;m</abbr></td>";
                             else 
                                 echo "——";
@@ -92,6 +107,70 @@
                   
 ?>
 
+            </tbody>
+            </table>
+<?php
+        }
+       
+        if(isset($hasImageSize))
+        {
+?>
+            <table cellspacing="0" summary="Image dimensions. There is one row of column headers, and one column of row headers describing the dimension">
+            <thead></thead>
+            <tbody>
+            <tr class="even">
+            <th class="wave_header_cell">Channel</th>
+            <th class="wave_header_cell">Wavelength</th>
+            <th class="wave_header_cell"></th>
+            </tr>
+            
+<?php
+            $nextColor= "odd";
+            $counter = 0;
+            foreach($dimension as $dim)
+            {
+                if(isset($dim->Space) && strcmp($dim->Space,"wavelength") == 0)
+                {
+                    $counter++;
+?>
+                    <tr class="<?php echo $nextColor; ?>">
+                    <th scope="row">
+                    <?php
+                        echo $counter;
+                    ?>
+                    </th>
+                    
+                    <td>
+                    <?php
+                        if(isset($dim->Value))
+                        {
+                            echo $dim->Value;
+                            if(strcmp($dim->Unit, "nanometers")==0)
+                                echo "<abbr title=\"nanometers\">nm</abbr></td>";
+                            else if(strcmp($dim->Unit, "microns")==0)
+                                echo "<abbr title=\"microns\">&micro;m</abbr></td>";
+                            else 
+                                echo "——";
+                        }
+                        else
+                        {
+                            echo "——";
+                        }
+                    ?>
+                    </td>
+                    <td></td>
+                    </tr>
+                   
+<?php
+                   if(strcmp($nextColor,"odd")==0)
+                       $nextColor = "even";
+                   else
+                       $nextColor = "odd";
+                }
+            }
+
+?>
+          
             </tbody>
             </table>
 <?php
