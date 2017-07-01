74ca414ec06aa6e4039a7d083661dd8f0856e642
wawong <wawong@gmail.com>
2017-06-30 21:57:23 -0700

    Added homepage

--- CIL/application/views/templates/cil_footer2.php
+++ CIL/application/views/templates/cil_footer2.php
@@ -1,3 +1,35 @@
+<div class="container">
+<br/>
+<div class="row">
+    <div class="col-md-12">
+        <center>
+        <ul class="footer_nav">
+        <li><a class="survey_plain" href="http://www.cellimagelibrary.org/pages/about">About</a></li>
+        <li><a class="survey_plain" href="http://www.cellimagelibrary.org/pages/press">In the News</a></li>
+        <li><a class="survey_plain"href="http://www.cellimagelibrary.org/pages/license">Licensing</a></li>
+        <li><a class="survey_plain" href="http://www.cellimagelibrary.org/pages/notification">Notification of Use</a></li>
+        </ul>
+        </center>
+    </div>
+</div>
+<br/>
+<div class="row">
+    <div class="col-md-3">
+        <a href="http://ascb.org/" class="grid_3 suffix_1" target="_blank"><img alt="Ascb_logo" src="http://www.cellimagelibrary.org/pix/ascb_logo.gif?1328662159"></a>
+    </div>
+    <div class="col-md-3">
+        <a href="http://ccdb.ucsd.edu/" class="grid_3 suffix_1" id="ccdb_logo_banner" target="_blank"><img alt="Ccdb_logo_banner" src="http://www.cellimagelibrary.org//pix/ccdb_logo_banner.jpg?1315859566"></a>
+    </div>
+    <div class="col-md-3">
+        <a href="http://www.neuinfo.org" alt="Registered with NIF" id="nif_button" target="_blank" title="Registered with NIF"><img alt="Nif_button2" height="40" src="http://www.cellimagelibrary.org/pix/nif_button2.png?1332269892" width="125"></a>
+    </div>
+    <div class="col-md-1">
+        <a href="http://wokinfo.com/products_tools/multidisciplinary/dci/" alt="Data Citation Index" target="_blank" title="Data Citation Index"><img alt="Dci_button" height="70" id="dci" src="http://www.cellimagelibrary.org/pix/DCI_button.png?1361486234" width="70"></a>
+    </div>
+    <div class="col-md-2">
+        <a href="http://www.the-scientist.com/?articles.view/articleNo/31109/title/The-2011-Labby-Multimedia-Awards/" target="_blank"><img alt="Labbyrcw" src="http://www.cellimagelibrary.org/pix/labbyrcw.jpg?1314988772"></a>
+    </div>
+</div>
 </div>
 </body>
 </html>
