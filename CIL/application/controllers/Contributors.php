b5490d705ebff5e29eb325ca16b33433718880a8
wawong <wawong@gmail.com>
2017-07-25 19:19:32 -0700

    Added the contributor page

--- CIL/application/controllers/Contributors.php
+++ CIL/application/controllers/Contributors.php
@@ -9,10 +9,14 @@ class Contributors  extends CI_Controller
     public function Index()
     {
          $data['test'] = "test";
-            $this->load->view('templates/cil_header4', $data);
-            $this->load->view('temp/comming_soon', $data);
-            $this->load->view('templates/cil_footer2', $data);
-            return;
+         $scontributors = file_get_contents(getcwd()."/application/json_config/Contributors/contributors.json");
+         $contributors = json_decode($scontributors);
+         $data['contributors'] = $contributors;
+         
+         $this->load->view('templates/cil_header4', $data);
+         $this->load->view('contributors/contributor_display', $data);
+         $this->load->view('templates/cil_footer2', $data);
+         return;
     }
     
     
---
+++ CIL/application/views/contributors/contributor_display.php
@@ -0,0 +1,87 @@
+<?php
+
+//var_dump($contributors);
+
+
+?>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h2>Image Contributors to The Cell</h2>
+        </div>
+    </div>
+    <!-- <div class="row">
+        <div class="col-md-12">
+            <div class="caption">
+                <a href="">test</a>
+            </div>
+        </div>
+    </div> -->
+<?php
+
+      $array =  $contributors->Contributors;
+      $count = count($array);
+      $i=0;
+      //echo "COUNT:".$count;
+      //$count = 10;
+      while($i < $count)
+      {
+          
+?>
+     <div class="row top-buffer">
+        <div class="col-md-3">
+         <?php
+            if($i < $count)
+            {
+                $contributor = $array[$i];
+                $url = "/images?k=".urlencode($contributor->Full_name)."&simple_search=Search";
+                echo "<p class=\"contributor\"><a href=\"".$url."\" target=\"_self\">".$contributor->Full_name." (".$contributor->Count.")" ."</a></p>";
+                $i++;
+                
+            } 
+         ?>
+        </div>
+         <div class="col-md-3">
+         <?php
+            if($i < $count)
+            {
+                $contributor = $array[$i];
+                $url = "/images?k=".urlencode($contributor->Full_name)."&simple_search=Search";
+                echo "<p class=\"contributor\"><a href=\"".$url."\" target=\"_self\">".$contributor->Full_name." (".$contributor->Count.")" ."</a></p>";
+                $i++;
+                
+            } 
+         ?>
+        </div>
+         <div class="col-md-3">
+         <?php
+            if($i < $count)
+            {
+                $contributor = $array[$i];
+                $url = "/images?k=".urlencode($contributor->Full_name)."&simple_search=Search";
+                echo "<p class=\"contributor\"><a href=\"".$url."\" target=\"_self\">".$contributor->Full_name." (".$contributor->Count.")" ."</a></p>";
+                $i++;
+                
+            } 
+         ?>
+        </div>
+         <div class="col-md-3">
+        <?php
+            if($i < $count)
+            {
+                $contributor = $array[$i];
+                $url = "/images?k=".urlencode($contributor->Full_name)."&simple_search=Search";
+                echo "<p class=\"contributor\"><a href=\"".$url."\" target=\"_self\">".$contributor->Full_name." (".$contributor->Count.")" ."</a></p>";
+                $i++;
+                
+            } 
+         ?>
+        </div>
+        
+     </div>
+<?php
+      }
+      
+?>
+    
+</div>
