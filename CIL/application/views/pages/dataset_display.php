da557e7a15ddfa44b72056e3b24dfd6af77fce93
wawong <wawong@gmail.com>
2017-09-14 15:52:25 -0700

    Added more details in dataset page

--- CIL/application/views/pages/dataset_display.php
+++ CIL/application/views/pages/dataset_display.php
@@ -26,11 +26,15 @@
                 
               </div>
               <div class="col-md-12">
-                <a href="/pages/project_20269" target="_self"><h3>Human U2OS cells - compound cell-painting experiment</h3></a><br/><b>CIL Project 20269</b>
+                <a href="/pages/project_20269" target="_self"><h3>Human U2OS cells - compound cell-painting experiment</h3></a><br/>
               </div>
               <div class="col-md-12">
-                <span style="font-size:12px;font-weight: normal;">Phenotypic profiling attempts to summarize multiparametric, feature-based analysis of cellular phenotypes of each sample so that similarities between profiles reflect similarities between samples. This image set provides a basis for testing image-based profiling methods wrt. to their ability to distinguish the effects of small molecules.</span>
+                        <b>Project ID:</b> P20269
               </div>
+              <div class="col-md-12 top-buffer">
+                <b>Description: </b><span style="font-size:12px;font-weight: normal;">Phenotypic profiling attempts to summarize multiparametric, feature-based analysis of cellular phenotypes of each sample so that similarities between profiles reflect similarities between samples. This image set provides a basis for testing image-based profiling methods wrt. to their ability to distinguish the effects of small molecules.</span>
+              </div>
+                
             </div>
         </div>
     </div>
@@ -53,10 +57,38 @@
                 </center>
             </div>
             <div class="col-md-10">
-                <a href="<?php echo "/project/".$project->ID; ?>" target="_self"><h3><?php echo $project->Name;  ?></h3></a><br/><b>CIL Project: <?php echo $project->ID  ?></b><br/>
+                <!-- <a href="<?php //echo "/project/".$project->ID; ?>" target="_self"><h3><?php //echo $project->Name;  ?></h3></a><br/><b>CIL Project: <?php //echo $project->ID  ?></b><br/>
                 <span style="font-size:12px;font-weight: normal;"><?php 
-                if(isset($project->Description))
-                echo $project->Description;  ?></span>
+                //if(isset($project->Description))
+                //echo $project->Description;  ?></span>  -->
+                <div class="row">
+                    <div class="col-md-12">
+                        <a href="<?php echo "/project/".$project->ID; ?>" target="_self"><h3><?php echo $project->Name;  ?></h3></a>
+                    </div>
+                    <div class="col-md-12">
+                        <br/>
+                    </div>
+                    <div class="col-md-12">
+                        <b>Project ID:</b> <?php echo $project->ID  ?></b>
+                    </div>
+                    <div class="col-md-12 top-buffer">
+                        <span style="font-size:12px;font-weight: normal;"><?php 
+                            if(isset($project->Description))
+                                echo "<b>Description:</b> ".$project->Description;  ?></span>
+                    </div>
+                    <div class="col-md-12 top-buffer">
+                        <span style="font-size:12px;font-weight: normal;"><?php 
+                            if(isset($project->Funding_agency))
+                                echo "<b>Funding agency:</b> ".$project->Funding_agency;  ?></span>
+                    </div>
+                    <div class="col-md-12 top-buffer">
+                        <span style="font-size:12px;font-weight: normal;"><?php 
+                            if(isset($project->Publication))
+                                echo "<b>Publication:</b> ".$project->Publication;  ?></span>
+                    </div>
+                </div>
+                
+                
             </div>
         </div>
     <hr>
