142b06f4b85f4126e1b0fccbecb2a88e6c3bded6
wawong <wawong@gmail.com>
2017-11-26 19:49:42 -0800

    Finished Paramecium interactive cell

--- CIL/application/views/main_page/paramecium.php
+++ CIL/application/views/main_page/paramecium.php
@@ -3,15 +3,25 @@
     <div class="col-md-7">
         <img id="paramecium_img" class="interactive_cell_width" src="/pic/interactive_cells/paramecium/default.png">
     </div>
-    <div class="col-md-3">
+    <div class="col-md-3 row_no_gap_right">
          <ul class="list-group">
-            <li id="cilia" class="list-group-item "><a href="#cilia" class="anchor_blue">Cilia</a></li>
-            <li id="contractile_vacuole" class="list-group-item"><a href="#contractile_vacuole " class="anchor_blue">Contractile vacuole </a></li>
-            <li id="cytoplasm" class="list-group-item"><a href="#cytoplasm " class="anchor_blue">Cytoplasm </a></li>
+            <li id="cilia" class="list-group-item "><a href="/images?k=paramecium+cilia&simple_search=Search" class="anchor_blue">Cilia</a></li>
+            <li id="contractile_vacuole" class="list-group-item "><a href="/images?k=paramecium+contractile+vacuole&simple_search=Search" class="anchor_blue">Contractile vacuole </a></li>
+            <li id="cytoplasm" class="list-group-item"><a href="/images?k=paramecium+cytoplasm&simple_search=Search" class="anchor_blue">Cytoplasm</a></li>
+            <li id="cytoproct" class="list-group-item"><a href="/images?k=paramecium+cytoproct&simple_search=Search" class="anchor_blue">Cytoproct</a></li>
+            <li id="food_vacuoles" class="list-group-item"><a href="/images?k=paramecium+food+vacuole&simple_search=Search" class="anchor_blue">Food vacuoles</a></li>
+            <li id="cytopharynx" class="list-group-item"><a href="/images?k=paramecium+cytopharynx&simple_search=Search" class="anchor_blue">Cytopharynx</a></li>
+            <li id="micronucleus" class="list-group-item"><a href="/images?k=paramecium+micronucleus&simple_search=Search" class="anchor_blue">Micronucleus</a></li>
+            <li id="macronucleus" class="list-group-item"><a href="/images?k=paramecium+macronucleus&simple_search=Search" class="anchor_blue">Macronucleus</a></li>
+            <li id="oral_groove" class="list-group-item"><a href="/images?k=paramecium+oral+apparatus&simple_search=Search" class="anchor_blue">Oral groove</a></li>
+            <li id="pellicle" class="list-group-item"><a href="/images?k=paramecium+pellicle&simple_search=Search" class="anchor_blue">Pellicle</a></li>
+            <li id="trichocysts" class="list-group-item"><a href="/images?k=trichocyst+paramecium&simple_search=Search" class="anchor_blue">trichocysts</a></li>
 
-      </ul>
+         
+         </ul>
         
     </div>
     <div class="col-md-2"></div>
 </div>
-cytoplasm
+
+
--- CIL/js/cil_interactive_cell.js
+++ CIL/js/cil_interactive_cell.js
@@ -12,6 +12,13 @@ $( "#cilia" ).mouseout(function() {
     
 });
 
+$( "#cilia" ).click(function() {
+    console.log('cilia click');
+    var win = window.open('/images?k=paramecium+cilia&simple_search=Search', '_self');
+    win.focus();
+    
+});
+
 
 $( "#contractile_vacuole" ).mouseover(function() {
     console.log('contractile_vacuole in');
@@ -26,6 +33,15 @@ $( "#contractile_vacuole" ).mouseout(function() {
     
 });
 
+$( "#contractile_vacuole" ).click(function() {
+    console.log('contractile_vacuole click');
+    var win = window.open('/images?k=paramecium+contractile+vacuole&simple_search=Search', '_self');
+    win.focus();
+    
+});
+
+
+
 $( "#cytoplasm" ).mouseover(function() {
     console.log('cytoplasm in');
     document.getElementById('cytoplasm').className  = 'list-group-item selected_blue';
@@ -38,5 +54,187 @@ $( "#cytoplasm" ).mouseout(function() {
     document.getElementById('paramecium_img').src = '/pic/interactive_cells/paramecium/default.png';
     
 });
+
+$( "#cytoplasm" ).click(function() {
+    console.log('cytoplasm click');
+    var win = window.open('/images?k=paramecium+cytoplasm&simple_search=Search', '_self');
+    win.focus();
+    
+});
+
+
+$( "#cytoproct" ).mouseover(function() {
+    console.log('cytoproct in');
+    document.getElementById('cytoproct').className  = 'list-group-item selected_blue';
+    document.getElementById('paramecium_img').src = '/pic/interactive_cells/paramecium/cytoproct.png';
+});
+
+$( "#cytoproct" ).mouseout(function() {
+    console.log('cytoproct out');
+    document.getElementById('cytoproct').className  = 'list-group-item';
+    document.getElementById('paramecium_img').src = '/pic/interactive_cells/paramecium/default.png';
+    
+});
+
+$( "#cytoproct" ).click(function() {
+    console.log('cytoproct click');
+    var win = window.open('/images?k=paramecium+cytoproct&simple_search=Search', '_self');
+    win.focus();
+    
+});
+
+
+$( "#food_vacuoles" ).mouseover(function() {
+    console.log('food_vacuoles in');
+    document.getElementById('food_vacuoles').className  = 'list-group-item selected_blue';
+    document.getElementById('paramecium_img').src = '/pic/interactive_cells/paramecium/food_vacuoles.png';
+});
+
+$( "#food_vacuoles" ).mouseout(function() {
+    console.log('food_vacuoles out');
+    document.getElementById('food_vacuoles').className  = 'list-group-item';
+    document.getElementById('paramecium_img').src = '/pic/interactive_cells/paramecium/default.png';
+    
+});
+
+$( "#food_vacuoles" ).click(function() {
+    console.log('food_vacuoles click');
+    var win = window.open('/images?k=paramecium+food+vacuole&simple_search=Search', '_self');
+    win.focus();
+    
+});
+
+
+
+$( "#cytopharynx" ).mouseover(function() {
+    console.log('cytopharynx in');
+    document.getElementById('cytopharynx').className  = 'list-group-item selected_blue';
+    document.getElementById('paramecium_img').src = '/pic/interactive_cells/paramecium/cytopharynx.png';
+});
+
+$( "#cytopharynx" ).mouseout(function() {
+    console.log('cytopharynx out');
+    document.getElementById('cytopharynx').className  = 'list-group-item';
+    document.getElementById('paramecium_img').src = '/pic/interactive_cells/paramecium/default.png';
+    
+});
+
+$( "#cytopharynx" ).click(function() {
+    console.log('cytopharynx click');
+    var win = window.open('/images?k=paramecium+cytopharynx&simple_search=Search', '_self');
+    win.focus();
+    
+});
+
+
+
+$( "#micronucleus" ).mouseover(function() {
+    console.log('micronucleus in');
+    document.getElementById('micronucleus').className  = 'list-group-item selected_blue';
+    document.getElementById('paramecium_img').src = '/pic/interactive_cells/paramecium/micronucleus.png';
+});
+
+$( "#micronucleus" ).mouseout(function() {
+    console.log('cytopharynx out');
+    document.getElementById('micronucleus').className  = 'list-group-item';
+    document.getElementById('paramecium_img').src = '/pic/interactive_cells/paramecium/default.png';
+    
+});
+
+$( "#micronucleus" ).click(function() {
+    console.log('micronucleus click');
+    var win = window.open('/images?k=paramecium+micronucleus&simple_search=Search', '_self');
+    win.focus();
+    
+});
+
+
+
+$( "#macronucleus" ).mouseover(function() {
+    console.log('macronucleus in');
+    document.getElementById('macronucleus').className  = 'list-group-item selected_blue';
+    document.getElementById('paramecium_img').src = '/pic/interactive_cells/paramecium/macronucleus.png';
+});
+
+$( "#macronucleus" ).mouseout(function() {
+    console.log('macronucleus out');
+    document.getElementById('macronucleus').className  = 'list-group-item';
+    document.getElementById('paramecium_img').src = '/pic/interactive_cells/paramecium/default.png';
+    
+});
+
+$( "#macronucleus" ).click(function() {
+    console.log('macronucleus click');
+    var win = window.open('/images?k=paramecium+macronucleus&simple_search=Search', '_self');
+    win.focus();
+    
+});
+
+
+
+$( "#oral_groove" ).mouseover(function() {
+    console.log('oral_groove in');
+    document.getElementById('oral_groove').className  = 'list-group-item selected_blue';
+    document.getElementById('paramecium_img').src = '/pic/interactive_cells/paramecium/oral_groove.png';
+});
+
+$( "#oral_groove" ).mouseout(function() {
+    console.log('oral_groove out');
+    document.getElementById('oral_groove').className  = 'list-group-item';
+    document.getElementById('paramecium_img').src = '/pic/interactive_cells/paramecium/default.png';
+    
+});
+
+$( "#oral_groove" ).click(function() {
+    console.log('oral_groove click');
+    var win = window.open('/images?k=paramecium+oral+apparatus&simple_search=Search', '_self');
+    win.focus();
+    
+});
+
+
+
+
+$( "#pellicle" ).mouseover(function() {
+    console.log('pellicle in');
+    document.getElementById('pellicle').className  = 'list-group-item selected_blue';
+    document.getElementById('paramecium_img').src = '/pic/interactive_cells/paramecium/pellicle.png';
+});
+
+$( "#pellicle" ).mouseout(function() {
+    console.log('pellicle out');
+    document.getElementById('pellicle').className  = 'list-group-item';
+    document.getElementById('paramecium_img').src = '/pic/interactive_cells/paramecium/default.png';
+    
+});
+
+$( "#pellicle" ).click(function() {
+    console.log('pellicle click');
+    var win = window.open('/images?k=paramecium+pellicle&simple_search=Search', '_self');
+    win.focus();
+    
+});
+
+
+$( "#trichocysts" ).mouseover(function() {
+    console.log('trichocysts in');
+    document.getElementById('trichocysts').className  = 'list-group-item selected_blue';
+    document.getElementById('paramecium_img').src = '/pic/interactive_cells/paramecium/trichocysts.png';
+});
+
+$( "#trichocysts" ).mouseout(function() {
+    console.log('trichocysts out');
+    document.getElementById('trichocysts').className  = 'list-group-item';
+    document.getElementById('paramecium_img').src = '/pic/interactive_cells/paramecium/default.png';
+    
+});
+
+$( "#trichocysts" ).click(function() {
+    console.log('trichocysts click');
+    var win = window.open('/images?k=trichocyst+paramecium&simple_search=Search', '_self');
+    win.focus();
+    
+});
+
 
 } );
