81734004d6d0db565f6afbaf8be2a406a718f160
Willy Wong <wawong@gmail.com>
2017-12-31 23:24:29 -0800

    Delete the old and unused browse/cellprocess function

--- CIL/application/controllers/Browse.php
+++ CIL/application/controllers/Browse.php
@@ -37,9 +37,11 @@ class Browse  extends CI_Controller
     }
     
     
+    
+   
     public function cellprocess($input="None")
     {
-        //$from = $page_num*$size;
+        
         $sutil = new CILServiceUtil2();
         $gutil = new GeneralUtil();
         
@@ -51,7 +53,6 @@ class Browse  extends CI_Controller
         {
             $page = intval($temp);
             $page = $page-1;
-            //echo "---------page after:".$page."<br/>";
             if($page < 0)
                 $page = 0;
         }
@@ -104,7 +105,6 @@ class Browse  extends CI_Controller
                 $data['size'] = $size;
                 $data['category'] = $category;
                 $data['context_name'] = $context_name;
-                //echo "<br/>Category".$category;
                 $data['page_num'] = $page;//$page_num;
                 
             ///////////////////////////pagination/////////////////////////////////
@@ -133,61 +133,7 @@ class Browse  extends CI_Controller
     }
     
     
-    /*
-    public function cellprocess($input="None",$page_num=0,$size=10)
-    {
-        $from = $page_num*$size;
-        $sutil = new CILServiceUtil2();
-        $gutil = new GeneralUtil();
-        
-       $sconfig = file_get_contents(getcwd()."/application/json_config/cell_processes/cell_process_summary.json");
-       $configJson = json_decode($sconfig);
-       if(strcmp($input,"None")==0)
-       {
-        
-        $data['summary'] = $configJson;
-        $data['category'] = "cellprocess";
-        $this->load->view('templates/cil_header4', $data);
-        $this->load->view('categories/cell_processes_display', $data);
-        $this->load->view('templates/cil_footer2', $data);
-       }
-       else //if(strcmp($input,"Actin%20Based%20Processes")==0)
-       {
-           $category = $input;
-           $input = str_replace("%20", " ", $input);
-           $context_name = "cellprocess";
-           $data['category_title'] = $input;
-           $data['cil_image_prefix'] = $this->config->item('cil_image_prefix');
-           
-           $this->load->view('templates/cil_header4', $data);
-           
-           
-           $queryFileName = $this->getQueryFileName($configJson, $input);
-           $query = file_get_contents(getcwd()."/application/json_config/cell_processes/".$queryFileName);
-             
-           $query_url =  $this->config->item('advanced_search')."?from=".$from."&size=".$size;
-           $response = $sutil->curl_get_data($query_url,$query);
-           
-           //echo $response;
-           $result = json_decode($response);
-           if($result->hits->total > 0)
-           {
-                $data['result'] = $result;
-                $data['total'] = $result->hits->total;
-                $data['size'] = $size;
-                
-                $data['category'] = $category;
-                $data['context_name'] = $context_name;
-                //echo "<br/>Category".$category;
-                $data['page_num'] = $page_num;
-                
-                
-                $this->load->view('categories/category_search_result_page', $data);
-           }
-           $this->load->view('templates/cil_footer2', $data);
-       }
-    }
-    */
+    
     
     /*
     public function cellcomponent($input="None",$page_num=0,$size=10)
