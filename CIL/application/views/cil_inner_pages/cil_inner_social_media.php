478265b2dc4a30ed3684fc2af0470cb24df8ba29
Willy Wong <wawong@gmail.com>
2018-02-13 08:29:35 -0800

    Fix issue #97 - tweet button

--- CIL/application/views/cil_inner_pages/cil_inner_social_media.php
+++ CIL/application/views/cil_inner_pages/cil_inner_social_media.php
@@ -43,11 +43,12 @@
     <div class="col-md-12">
 
         <div class='addthis_toolbox addthis_default_style'>
+       
         <a class='addthis_button_email'></a>
         <a class='addthis_button_linkedin'></a>
         <a class='addthis_button_stumbleupon_badge' su:badge:style='4' su:badge:width='20px'></a>
         <a class='addthis_button_facebook'></a> 
-        <a class='addthis_button_tweet'></a>&nbsp;&nbsp;&nbsp;&nbsp;
+        <a class='addthis_button_twitter'> <img src="/pic/twitter-icon.png" width="16px" height="16px" border="0" alt="tweet"></a> &nbsp;
         <a class='addthis_counter addthis_pill_style'></a>
         
         <script src="http://s7.addthis.com/js/250/addthis_widget.js#pubid=orloff" type="text/javascript">
