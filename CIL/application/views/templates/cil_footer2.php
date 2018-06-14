ba12014aa632d6a411ffd5536da55112453f5d61
Willy Wong <wawong@gmail.com>
2018-06-13 18:00:25 -0700

    Added the new footer

--- CIL/application/views/templates/cil_footer2.php
+++ CIL/application/views/templates/cil_footer2.php
@@ -1,4 +1,6 @@
-<div class="container">
+<!-- Footer -->
+<footer class="footer footer_background">
+<div class="container footer_background">
 <div class="row">
     <div class="col-md-12">
        <br/>
@@ -6,38 +8,88 @@
     </div>
 </div>
 
-<div class="row">
+<div class="row footer_background">
+    <div class="col-md-12">
+        <div class="row footer-row">
+            <div class="col-md-2">
+                <a class="cil_16_font" href="/pages/about">About</a>
+            </div>
+            <div class="col-md-2">
+                <a class="cil_16_font" href="/pages/license">Licensing</a>
+            </div>
+            <div class="col-md-3">
+                <a class="cil_16_font" href="/pages/notification">Notification of Use</a>
+            </div>
+            <div class="col-md-2">
+                <a class="cil_16_font" href="/pages/cell_illustration">Interactive Cells</a>
+            </div>
+            <div class="col-md-2">
+                <a class="cil_16_font" href="https://github.com/slash-segmentation/CIL_PHP_Website" target="_blank">Open source</a>
+            </div>
+            
+        </div>
+    </div>
+</div>
+<div class="row footer_background top-buffer">
     <div class="col-md-3">
         <center>
-            <!-- <a href="http://ascb.org/" class="grid_3 suffix_1" target="_blank"><img alt="Ascb_logo" src="http://www.cellimagelibrary.org/pix/ascb_logo.gif?1328662159"></a> -->
             <a href="http://ascb.org/" class="grid_3 suffix_1" target="_blank"><img alt="Ascb_logo" src="/pix/ascb_logo.gif?<?php echo uniqid(); ?>"></a>
         </center>
     </div>
-    <div class="col-md-3">
-        <center>
-            <!-- <a href="http://ccdb.ucsd.edu/" class="grid_3 suffix_1" id="ccdb_logo_banner" target="_blank"><img alt="Ccdb_logo_banner" src="http://www.cellimagelibrary.org//pix/ccdb_logo_banner.jpg?1315859566"></a> -->
-            <a href="http://ccdb.ucsd.edu" class="grid_3 suffix_1" id="ccdb_logo_banner" target="_blank"><img alt="Ccdb_logo_banner" src="/pix/ccdb_logo_banner.jpg?<?php echo uniqid(); ?>"></a>
-        </center>
-    </div>
+    
     <div class="col-md-2">
-        <center>
-            <!-- <a href="http://www.neuinfo.org" alt="Registered with NIF" id="nif_button" target="_blank" title="Registered with NIF"><img alt="Nif_button2" height="40" src="http://www.cellimagelibrary.org/pix/nif_button2.png?1332269892" width="125"></a> -->
+        <center>         
             <a href="http://www.neuinfo.org" alt="Registered with NIF" id="nif_button" target="_blank" title="Registered with NIF"><img alt="Nif_button2" height="40" src="/pix/nif_button2.png?<?php echo uniqid(); ?>" width="125"></a>
         </center>
     </div>
     <div class="col-md-2">
         <center>
-            <!-- <a href="http://wokinfo.com/products_tools/multidisciplinary/dci/" alt="Data Citation Index" target="_blank" title="Data Citation Index"><img alt="Dci_button" height="70" id="dci" src="http://www.cellimagelibrary.org/pix/DCI_button.png?1361486234" width="70"></a> -->
             <a href="http://wokinfo.com/products_tools/multidisciplinary/dci/" alt="Data Citation Index" target="_blank" title="Data Citation Index"><img alt="Dci_button" height="70" id="dci" src="/pix/DCI_button.png?<?php echo uniqid(); ?>" width="70"></a>
         </center>
     </div>
     <div class="col-md-2">
         <center>
-            <!-- <a href="http://www.the-scientist.com/?articles.view/articleNo/31109/title/The-2011-Labby-Multimedia-Awards/" target="_blank"><img alt="Labbyrcw" src="http://www.cellimagelibrary.org/pix/labbyrcw.jpg?1314988772"></a> -->
             <a href="http://www.the-scientist.com/?articles.view/articleNo/31109/title/The-2011-Labby-Multimedia-Awards/" target="_blank"><img alt="Labbyrcw" src="/pix/labbyrcw.jpg?<?php echo uniqid(); ?>"></a>
+        </center>
+    </div>
+    <div class="col-md-3">
+        <address>
+            <strong>Center for Research in Biological Systems</strong><br>
+            Basic Science Building, Room 1000 <br>
+            University of California, San Diego<br>
+            9500 Gilman Drive<br>
+            La Jolla, CA 92093-0608, USA<br>
+            
+            
+            <strong>Voice</strong>: (858) 534-0276<br>
+            <strong>Fax</strong>: (858) 534-7497<br>
+            <strong>Email</strong>: dorloff@ncmir.ucsd.edu
+            
+        </address>
+    </div>
+</div>
+    <div class="row footer_background top-buffer">
+        <div class="col-md-12"><br/></div>
+    </div>
+
+    
+</div>
+</footer>
+<!-- Footer -->
+<footer class="footer footer_background2">
+<div class="container footer_background2">
+<div class="row">
+    <div class="col-md-12">
+        <center>
+        <!-- <span class="cil_16_font_black">Cell Image Library</span> -->
+            <a href="http://ucsd.edu" target="_blank">
+                <img src="/pix/icon_ucsd.png" />
+            </a>
         </center>
     </div>
 </div>
 </div>
+</footer>
+
 </body>
 </html>
