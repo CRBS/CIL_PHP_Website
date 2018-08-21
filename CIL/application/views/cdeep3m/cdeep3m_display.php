952652ffda4fd37baa3b957f69efd042f30e0160
Willy Wong <wawong@gmail.com>
2018-08-21 16:19:01 -0700

    Updated CDeep3M

--- CIL/application/views/cdeep3m/cdeep3m_display.php
+++ CIL/application/views/cdeep3m/cdeep3m_display.php
@@ -25,17 +25,108 @@
         <div class="col-md-6">
             <img src="/pix/uCT_Hippo0112.png" width="100%" />
         </div>
+        
         <div class="col-md-6">
-            <p class="cil_description">
+            <div class="row">
+            <div class="col-md-12">
+                <span class="cil_title2">Description</span>
+            </div>
+            <div class="col-md-12  cil_description">
+            
             As biological imaging datasets increase in size, deep neural networks are considered vital tools for efficient image segmentation. While a number of different network architectures have been developed for segmenting even the most challenging biological images, community access is still limited by the difficulty of setting up complex computational environments and processing pipelines, and the availability of compute resources. Here, we address these bottlenecks, providing a ready-to-use image segmentation solution for any lab, with a pre-configured, publicly available, cloud-based deep convolutional neural network on Amazon Web Services (AWS). We provide simple instructions for training and applying CDeep3M for segmentation of large and complex 2D and 3D microscopy datasets of diverse biomedical imaging modalities. <a href="https://www.biorxiv.org/content/early/2018/06/21/353425" target="_blank">Read more.</a>
-            </p>
-        </div>
-        <br/><br/>
-        <div class="class-md-12 cil_description">
-            <center><a href="https://cildata.crbs.ucsd.edu/media/cdeep3m/XRM_Nuclei_TrainedNet_062018.zip" target="_self">Download</a></center>
-        </div>
-    </div>
+            
+            <br/><br/>
+            </div>
+            <div class="col-md-12">
+                <span class="cil_title2">Technical Details</span>
+            </div>
+            <div class="col-md-12">
+               <div class="biological_sources">
+
+            <dl>
     
+    
+<!------------------NCBI Organism Classification---------------------->
+
+                    <dt><span class="cil_small_title">Trained model</span></dt>
+                    <dd class="eol_dd">
+                    <span>
+                        Nucleus segmentation
+                    </span>
+                    </dd>
+                    
+
+
+<!------------------End NCBI Organism Classification---------------------->    
+    
+
+<!------------------Cell type---------------------->
+                
+                <!-- <dt><b>Cell Type</b></dt> -->
+                <dt><span class="cil_small_title">Sample</span></dt>
+                <dd class="eol_dd">Hippocampus Mouse Brain Stained for SBEM</dd>
+            
+<!------------------End Cell type---------------------->        
+        
+
+
+
+<!------------------Cell LINE---------------------->
+<!------------------End Cell Line---------------------->
+
+
+
+
+
+
+
+<!------------------Cell Components---------------------->
+                <!-- <dt><b>Cellular Component</b></dt> -->
+                <dt><span class="cil_small_title">Modality</span></dt>
+                <dd class="eol_dd">
+                <span>
+                X-Ray micro CT
+                </span>
+                </dd>
+
+<!------------------End Cellular Component----------------------> 
+
+<!------------------Cell Components---------------------->
+                <!-- <dt><b>Cellular Component</b></dt> -->
+                <dt><span class="cil_small_title">Objective</span></dt>
+                <dd class="eol_dd">
+                <span>
+                40x
+                </span>
+                </dd>
+
+<!------------------End Cellular Component----------------------> 
+
+<!------------------Cell Components---------------------->
+                <!-- <dt><b>Cellular Component</b></dt> -->
+                <dt><span class="cil_small_title">Voxelsize</span></dt>
+                <dd class="eol_dd">
+                <span>
+                0.4159µm
+                </span>
+                </dd>
+
+<!------------------End Cellular Component----------------------> 
+        
+
+</dl>
+</div>     
+            </div>
+            <br/><br/>
+            
+            <div class="col-md-12 cil_description">
+                <center><a class="button mini" href="https://cildata.crbs.ucsd.edu/media/cdeep3m/XRM_Nuclei_TrainedNet_062018.zip" target="_self">Download</a></center>
+            </div>
+            <br/><br/> 
+           
+           </div>
+            </div>
+        </div>
     
 
 </div>
