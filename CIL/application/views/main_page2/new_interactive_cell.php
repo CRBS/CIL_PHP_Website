e56f272644a98bed3a714476ae1293c29bcc87a4
Willy Wong <wawong@gmail.com>
2018-11-08 18:39:00 -0800

    Set the interactive cell display based on the data

--- CIL/application/views/main_page2/new_interactive_cell.php
+++ CIL/application/views/main_page2/new_interactive_cell.php
@@ -1,7 +1,14 @@
 <?php
-
-    //include_once 'paramecium.php';
-    //include_once 'eukaryotic.php';
-    include_once 'escherichia.php';
+    
+    $icell = "";
+    if(isset($settings->_source->Home_page->{"Interactive cells"}))
+        $icell = $settings->_source->Home_page->{"Interactive cells"};
+       
+    if(strcmp($icell,"Paramecium")==0)
+        include_once 'paramecium.php';
+    else if(strcmp($icell,"Eukaryotic")==0)
+        include_once 'eukaryotic.php';
+    else
+        include_once 'escherichia.php';
 ?>
 
