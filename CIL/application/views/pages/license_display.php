2fc9c2bf30f2d5b7b195f7c11aa822e3104d7146
Willy Wong <wawong@gmail.com>
2019-04-10 19:28:14 -0700

    Updated the license page

--- CIL/application/views/pages/license_display.php
+++ CIL/application/views/pages/license_display.php
@@ -19,7 +19,7 @@
             <span class="about_page_font">Our goals include allowing for the most effective use of the data (without attribution, large data sets can be used easily, furthering scientific discovery), and reaching the greatest number of users.</span>
             <div id="published_images"></div>
             <span class="cil_title">Published Images</span>
-            <span class="about_page_font"><em>Please note:</em> While The Cell welcomes previously published images, if you are submitting images that have previously been published, you must have the right to distribute the image or the licensing of the publishing journal must allow for it. Please check with your publisher to determine if this is the case. Many publishers, like the ASCB, which publishes the <em>Molecular Biology of the Cell</em>, allow authors to retain copyright. However, even these publishers may limit distribution of the work. These limitations could relate to intended use (commercial and/or educational), alteration, and attribution. Publishers may also choose to use a Creative Commons license that allows for some types of re-use without permission. Authors should confirm the rights they have related to published images before selecting the appropriate option. Information on some publishers' licenses is provided here for your convenience. Please note that many journals have a waiting period before images may be used.</span>
+            <span class="about_page_font"><em>Please note:</em> While The Cell welcomes previously published images, if you are submitting images that have previously been published, you must have the right to distribute the image or the licensing of the publishing journal must allow for it. Please check with your publisher to determine if this is the case. Many publishers, like the ASCB, which publishes the Molecular Biology of the Cell, allow authors to retain copyright. However, even these publishers may limit distribution of the work. These limitations could relate to intended use (commercial and/or educational), alteration, and attribution. Publishers may also choose to use a Creative Commons license that allows for some types of re-use without permission. Authors should confirm the rights they have related to published images before selecting the appropriate option.</span>
             <table class="licensing_table">
             <tbody><tr>
             <th>Publication</th>
@@ -62,21 +62,117 @@
             <span class="cil_title">Unpublished Images</span>
             <span class="about_page_font">If you have not published the images you wish to contribute, and you hold the copyright, you may select one of the licenses described in the Licensing section below.</span>
             <div id="licenses"></div>
-            <span class="cil_title">Licenses</span>
-            <span class="about_page_font">These options will be available during image submission. The license you choose will then be noted in The Cell on the detailed image page.</span>
+            <span class="cil_title">License Conditions</span>
+            <span class="about_page_font">Creators choose a set of conditions they wish to apply to their work.</span>
             <ol class="license_details_list">
             <li class="license_details_list">
             <div class="license_option">
             <div class="license_option_header_container">
             <div id="public_domain"></div>
-            <img alt="Pd" class="license_img" height="35" src="/pix/pd.gif?1286375725" width="35">
+            <!-- <img alt="Pd" class="license_img" height="35" src="/pix/pd.gif?1286375725" width="35"> -->
+            
+            <img alt="Pd" class="license_img" height="35" src="/pix/public_domain.jpg?<?php echo uniqid(); ?>" >
             <h4 class="license_option_name_public_domain">Public Domain</h4>
             </div>
             <div class="license_option_description_container">
-            <p class="license_english_description">Public Domain – If you own the copyright and want to dedicate the work to the public domain use this one. If you want to certify that the work is already in the public domain (for example, because all authors are U.S. Federal Employees) use this one. This will allow anyone to use or alter the image any way desired, including for commercial purposes.</p>
-            <h4 class="license_option_legal_name">This is the Creative Commons: Copyright-Only Dedication (based on United States law) or Public Domain Certification.</h4>
-            <p class="license_description">The person or persons who have associated work with this document (the "Dedicator" or "Certifier") hereby either (a) certifies that, to the best of his knowledge, the work of authorship identified is in the public domain of the country from which the work is published, or (b) hereby dedicates whatever copyright the dedicators holds in the work of authorship (the "Work") to the public domain.</p>
-            <a href="http://creativecommons.org/choose/publicdomain-3?title=&amp;copyright_holder=" class="license_view_links" target="_blank">Learn more</a>
+            <p class="license_english_description">The Public Domain Mark is for works that are no longer restricted by copyright and can be freely used by others.</p>
+
+            </div>
+            </div>
+            </li>
+            <li class="license_details_list">
+            <div class="license_option">
+            <div class="license_option_header_container">
+            <div id="attribution_cc_by"></div>
+            <!-- <img alt="Attribution_cc_by" class="license_img" src="/pix/attribution_cc_by.png?1286375725"> -->
+            <img alt="Attribution_cc_by" height="35" class="license_img" src="/pix/attribution.jpg?<?php echo uniqid(); ?>">
+            
+            <h4 class="license_option_name_public_domain">Attribution (by)</h4>
+            </div>
+            <div class="license_option_description_container">
+            <p class="license_english_description">This requires that others who use your work in any way must give you credit the way you request, but not in a way that suggests you endorse them or their use. If they want to use your work without giving you credit or for endorsement purposes, they must get your permission first.</p>
+
+            </div>
+            </div>
+            </li>
+            
+            <!---------ShareAlike------------------->
+            <li class="license_details_list">
+            <div class="license_option">
+            <div class="license_option_header_container">
+            <div id="attribution_nc_sa"></div>
+            <img alt="Attribution_nc_sa" height="35" class="license_img" src="/pix/share_alike.jpg?<?php echo uniqid(); ?>">
+            <h4 class="license_option_name_public_domain">ShareAlike (sa)</h4>
+            </div>
+            <div class="license_option_description_container">
+            <p class="license_english_description">This allows others to copy, distribute, display, perform, and modify your work, as long as they distribute any modified work on the same terms. If they want to distribute modified works under other terms, they must get your permission first.</p>
+            </div>
+            </div>
+            </li>
+            <!---------End ShareAlike------------------->
+            
+            <!---------non_commercial------------------->
+            <li class="license_details_list">
+            <div class="license_option">
+            <div class="license_option_header_container">
+            <div id="attribution_nc_sa"></div>
+            <img alt="Attribution_nc_sa" height="35" class="license_img" src="/pix/non_commercial.jpg?<?php echo uniqid(); ?>">
+            <h4 class="license_option_name_public_domain">NonCommercial (nc)</h4>
+            </div>
+            <div class="license_option_description_container">
+            <p class="license_english_description">This let others copy, distribute, display, perform, and (unless you have chosen NoDerivatives) modify and use your work for any purpose other than commercially unless they get your permission first.</p>
+            </div>
+            </div>
+            </li>
+            <!---------End non_commercial------------------->
+            
+            <!---------NoDerivatives ------------------->
+            <li class="license_details_list">
+            <div class="license_option">
+            <div class="license_option_header_container">
+            <div id="attribution_nc_sa"></div>
+            <img alt="Attribution_nc_sa" height="35" class="license_img" src="/pix/non_commercial.jpg?<?php echo uniqid(); ?>">
+            <h4 class="license_option_name_public_domain">NoDerivatives (nd)</h4>
+            </div>
+            <div class="license_option_description_container">
+            <p class="license_english_description">This lets others copy, distribute, display and perform only original copies of your work. If they want to modify your work, they must get your permission first.</p>
+            </div>
+            </div>
+            </li>
+            <!---------End NoDerivatives ------------------->
+            
+            
+            <li class="license_details_list">
+            <div class="license_option">
+            <div class="license_option_header_container">
+            <div id="copyright"></div>
+            <img alt="Copyright" class="license_img" height="30" src="/pix/copyright.jpg?<?php echo uniqid(); ?>" width="30">
+            <h4 class="license_option_name_copyright">Copyright</h4>
+            </div>
+            <div class="license_option_description_container">
+            <p class="license_english_description">This requires others get your permission for any use of the work.</p>
+            
+            
+            </div>
+            </div>
+            </li>
+            </ol>
+            <p>
+            <div class="about_page_font">For more information please see the Creative Commons website at <a href="https://creativecommons.org" target="_blank">https://creativecommons.org</a> </div>
+            <p class="about_page_font">
+            The Cell Image Library allows for the combination of the above license conditions as noted below. These options will be available during image submission. The license you choose will then be noted in The Cell on the detailed image page.    
+            </p>
+            <ol class="license_details_list">
+            <li class="license_details_list">
+            <div class="license_option">
+            <div class="license_option_header_container">
+            <div id="public_domain"></div>
+            <img alt="Pd" class="license_img" src="/pix/public_domain.jpg?<?php echo uniqid(); ?>" height="35">
+            <h4 class="license_option_name_public_domain">Public Domain</h4>
+            </div>
+            <div class="license_option_description_container">
+            <p class="license_english_description">Public Domain – If you own the copyright and want to dedicate the work to the public domain use this one. If you want to certify that the work is already in the public domain (for example, because all authors are U.S. Federal Employees) use this one.</p>
+
             </div>
             </div>
             </li>
@@ -86,15 +182,10 @@
             <div id="attribution_cc_by"></div>
             <img alt="Attribution_cc_by" class="license_img" src="/pix/attribution_cc_by.png?1286375725">
             <div class="attr_cc_by">
-            <h4 class="license_option_name_cc_by">Creative Commons; Attribution Required; Alteration, Dissemination and Commercial Use Allowed</h4>
+            <h4 class="license_option_name_cc_by">Creative Commons; Attribution Required</h4>
             </div>
             </div>
-            <div class="license_option_description_container">
-            <p class="license_english_description">Creative Commons; Attribution Required; Alteration, Dissemination and Commercial Use Allowed – If you own the copyright and want to make sure that any use of the work is credited to you, then use this one. This will also allow others to distribute, alter, and use the work commercially.</p>
-            <h4 class="license_option_legal_name">This is the Creative Commons: Attribution cc by</h4>
-            <p class="license_description">This license lets others distribute, remix, tweak, and build upon your work, even commercially, as long as they credit you for the original creation. This is the most accommodating of licenses offered, in terms of what others can do with your works licensed under Attribution.</p>
-            <a href="http://creativecommons.org/licenses/by/3.0" class="license_view_links" target="_blank">View License Deed</a> | <a href="http://creativecommons.org/licenses/by/3.0/legalcode" class="license_view_links" target="_blank">View Legal Code</a>
-            </div>
+            
             </div>
             </li>
             <li class="license_details_list">
@@ -102,31 +193,37 @@
             <div class="license_option_header_container">
             <div id="attribution_nc_sa"></div>
             <img alt="Attribution_nc_sa" class="license_img" src="/pix/attribution_nc_sa.png?1286375725">
-            <h4 class="license_option_name">Creative Commons; Attribution Required; Commercial Use Prohibited</h4>
+            <h4 class="license_option_name">Creative Commons; Attribution Required; NonCommercial; ShareAlike</h4>
             </div>
-            <div class="license_option_description_container">
-            <p class="license_english_description">Creative Commons; Attribution Required; Commercial Use Prohibited – If you own the copyright, want to be credited for all uses, but do not wish to allow commercial uses, use this one. This will also allow others to distribute and alter the work. All users must then distribute the work and any modified works with these same conditions.</p>
-            <h4 class="license_option_legal_name">This is the Creative Commons: Attribution Non-Commercial Share Alike cc by-nc-sa</h4>
-            <p class="license_description">This license lets others remix, tweak, and build upon your work non-commercially, as long as they credit you and license their new creations under the identical terms. Others can download and redistribute your work and they can also translate, make remixes, and produce new work based on your work. All new work based on yours will carry the same license, so any derivatives will also be non-commercial in nature.</p>
-            <a href="http://creativecommons.org/licenses/by-nc-sa/3.0" class="license_view_links" target="_blank">View License Deed</a> | <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/legalcode" class="license_view_links" target="_blank">View Legal Code</a>
-            </div>
+            
             </div>
             </li>
+            
+            <li class="license_details_list">
+            <div class="license_option">
+            <div class="license_option_header_container">
+            <div id="attribution_nc_sa"></div>
+            <img alt="Attribution_nc_sa" class="license_img" height="30" src="/pix/cc_nc_nd.jpg?1286375725">
+            <h4 class="license_option_name">Creative Commons; Attribution Required; NonCommercial; NoDerivatives</h4>
+            </div>
+            
+            </div>
+            </li>
+            
             <li class="license_details_list">
             <div class="license_option">
             <div class="license_option_header_container">
             <div id="copyright"></div>
-            <img alt="Copyright" class="license_img" height="30" src="/pix/copyright.jpg?1286375725" width="30">
-            <h4 class="license_option_name_copyright">Copyrighted. Use requirements to be specified upon request of the copyright holder.</h4>
+            <img alt="Copyright" class="license_img" src="/pix/copyright.jpg?1286375725" width="30" height="30">
+            <h4 class="license_option_name_copyright">Copyrighted. Use requirements to be specified upon request of the copyright holder</h4>
             </div>
             <div class="license_option_description_container">
-            <p class="license_english_description">Copyrighted. Use requirements to be specified upon request of the copyright holder.– If you own the copyright and want to continue to control use of your work, but  grant permission to use it in The Cell only, use this one.</p>
-            <h4 class="license_option_legal_name">Copyright Protected</h4>
-            <p class="license_description">This image/video/animation is copyright-protected. Any public or private use of this image is subject to prevailing copyright laws. Please contact the content provider of this image for permission requests.</p>
+                <p>
             </div>
             </div>
             </li>
             </ol>
+            <p>
             <div id="disclaimer"></div>
             <span class="cil_title">Disclaimer</span>
             <span class="about_page_font">Any company, product and service names used in this web site are for identification purposes only. All trademarks and registered trademarks are the property of their respective owners.</span>
