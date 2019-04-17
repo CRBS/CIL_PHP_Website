c8965655a24e29560c4c45931174a34ff962e7c8
Willy Wong <wawong@gmail.com>
2019-04-16 19:00:46 -0700

    Added the nucleus

--- CIL/application/controllers/Browse.php
+++ CIL/application/controllers/Browse.php
@@ -1576,6 +1576,11 @@ class Browse  extends CI_Controller
                              $data['centered_interactive_cell'] = true;
                              $this->load->view('main_page2/nucleolus', $data);
                          }
+                         else if(strcasecmp($input, "Nucleus") == 0)
+                         {
+                             $data['centered_interactive_cell'] = true;
+                             $this->load->view('main_page2/nucleus', $data);
+                         }
 
                          $this->load->view('categories2/category_search_result_page', $data);
                         }
--- CIL/application/views/main_page2/nucleus.php
+++ CIL/application/views/main_page2/nucleus.php
@@ -1,3 +1,8 @@
+<?php
+
+if(!isset($centered_interactive_cell))
+{
+?>
 <span class="cil_title">Explore <i>Nucleus</i></span>
 <div class="row">
     <div class="col-md-7">
@@ -14,4 +19,87 @@
     </div>
     <div class="col-md-2"></div>
 </div>
+<?php
+}
+else
+{
+?>
+<div class="container">
+<div class="row">
+    
+    <div class="col-md-12">
+        <div class="row">
+            <div class="col-md-12"><span class="cil_title">Nucleus</span></div>
+            <div class="col-md-5">
+                <img id="nucleus_img" class="interactive_cell_width" src="/pic/interactive_cells/nucleus/nucleus_main.jpg">
+            </div>
+            
+            
+            <div class="col-md-7">
+                
+                <div class="row">
+                    <div class="col-md-12">
+                        <ul class="list-group"> 
+                           <li id="nucleus_chromatin_id" class="list-group-item"><a href="/images?k=chromatin&simple_search=Search" class="anchor_blue">Chromatin</a></li>
+                           <li id="nuclear_envelope_id" class="list-group-item"><a href="/images?k=%22nuclear+envelope%22&simple_search=Search" class="anchor_blue">Nuclear envelope</a></li>
+                           <li id="nuclear_pore_id" class="list-group-item"><a href="/images?k=%22nuclear+pore%22&simple_search=Search" class="anchor_blue">Nuclear pore</a></li>
+                           <li id="nucleus_nucleolus_id" class="list-group-item"><a href="/images?k=microtubule&simple_search=Search" class="anchor_blue">Nucleolus</a></li>
+                        </ul>
+                        <hr>
+                    </div>
+                    <div class="col-md-12">
+                        <span style="font-size:12px;font-weight: bold;">Description:</span>
+                        <span style="font-size:12px;font-weight: normal;">The nucleus is the major site of gene expression in eukaryotic cells, separating the nuclear genome from cytoplasmic activities. Packed inside the membrane-bound nucleus are the chromosomes, consisting of long strands of DNA bound with a variety of proteins. The nucleus is the major site of transcription, where the genes carried on the chromosomes are copied into RNA sequences. These primary RNA sequences are processed in the nucleus, modified by the enzymatic addition of caps and tails, as well as splicing together of genetic sequences. Functional RNA molecules exit the nucleus through nuclear pores, and assemble in the cytoplasm on ribosomes where they are used to translate the RNA sequence information into chains of amino acids. Gene expression is controlled in the nucleus by a variety of means, including differential packing of chromatin and the number and type of transcription factors. A special region of the nucleus called the nucleolus is the site of ribosome synthesis.</span>
+                    </div>
+                    <div class="col-md-12"><br/></div>
+                    <div class="col-md-6">
+                        <div class="row">
+                            <div class="col-md-12"><span style="font-size:12px;font-weight: bold;">Related Molecular Functions</span><br/></div>
+                            <div class="col-md-12"><br/></div>
+                            <div class='col-md-12'>
+                                <div style="float: left; font-size:11px;font-weight: normal;">
+                                    <ul>
+                                        <li type="square"><a class="anchor_blue" href="/images?k=Phosphoserine+binding&simple_search=Search">Phosphoserine binding</a></li>
+                                        <li type="square"><a class="anchor_blue" href="/images?k=GTPase+activity&simple_search=Search">GTPase activity</a></li>
+                                        <li type="square"><a class="anchor_blue" href="/images?k=Enzyme+binding&simple_search=Searchg">Enzyme binding</a></li>
+                                        <li type="square"><a class="anchor_blue" href="/images?k=Phosphatidylinositol-4-phosphate+binding&simple_search=Search">Phosphatidylinositol-4-phosphate binding</a></li>
+                                        <li type="square"><a class="anchor_blue" href="/images?k=Nucleotide+binding&simple_search=Search">Nucleotide binding</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="row">
+                            <div class="col-md-12"><span style="font-size:12px;font-weight: bold;">Related Biological Processes</span></div>
+                            <div class="col-md-12"><br/></div>
+                            <div class='col-md-12'>
+                                <div style="float: left; font-size:11px;font-weight: normal;">
+                                    <ul>
+                                        <li type="square"><a class="anchor_blue" href="/images?k=Mitosis&simple_search=Search">Mitosis</a></li>
+                                        <li type="square"><a class="anchor_blue" href="/images?k=Nucleus+organization&simple_search=Search">Nucleus organization</a></li>
+                                        <li type="square"><a class="anchor_blue" href="/images?k=Macronuclear+organization&simple_search=Search">Macronuclear organization</a></li>
+                                        <li type="square"><a class="anchor_blue" href="/images?k=Organelle+organization&simple_search=Search">Organelle organization</a></li>
+                                        <li type="square"><a class="anchor_blue" href="/images?k=Cytoplasm+organisation&simple_search=Search">Cytoplasm organisation</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+            
+        </div>
+    </div>
+    
+     <div class="col-md-12"><br/><br/></div>
+</div>
+</div>
+
+
+<?php 
+} 
+?>
+
 
