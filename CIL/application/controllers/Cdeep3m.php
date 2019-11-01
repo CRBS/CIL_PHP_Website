823b5e16f5390e0c849b2a7302fec4bc33a9da69
Willy Wong <wawong@gmail.com>
2019-11-01 15:17:22 -0700

    Added a new image to the CDeep3m page

--- CIL/application/controllers/Cdeep3m.php
+++ CIL/application/controllers/Cdeep3m.php
@@ -37,6 +37,8 @@ class Cdeep3m  extends CI_Controller
         array_push($image_array, "CCDB_8192");
         array_push($image_array, "CCDB_8246");
         array_push($image_array, "CIL_50584");
+        array_push($image_array,"CIL_50585");
+        
         $data['image_array'] = $image_array;
         $this->load->view('templates/cil_header4', $data);
         $this->load->view('cdeep3m/prp_demo_display', $data);
