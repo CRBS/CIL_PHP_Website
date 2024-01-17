b1854ce7230910a1fbe4478632851d44176fc62c
Willy Wong <wawong@gmail.com>
2024-01-17 15:48:42 -0800

    added a new dataset

---
+++ CIL/application/views/pages/project_20483_display.php
@@ -0,0 +1,70 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-5">
+            <img src="/pix/P20483x512.jpg" width="100%">
+        </div>
+        <div class="col-md-7">
+            <span class="cil_title">Pediatric inflammatory bowel disease (IBD)</span>
+            <!-- <span style="font-size:12px;font-weight: normal;"><br><b>Description of biological application</b><br> -->
+            <span class="cil_14_bold_no_color">Description</span><br/>
+<span class="cil_13_no_color ">The dataset consists of 951 tissue section scans taken from 167 H&E-stained whole slides from 18 pediatric inflammatory bowel disease (IBD) patients. Slide section scans are provided as-is without any initial image pre-processing, normalization, or cropping. Labels are provided at the patient, slide (anatomic site), and section (tissue slice) levels. Each slide is labelled with biopsy site and normal or abnormal classification per the surgical pathology report. Each tissue section scan from an abnormal slide is further classified as normal or abnormal by pathologist. Files may be viewed and annotated using standard, opensource slide-viewing software, such as Aperio ImageScope, OpenSlide​​, TiffSlide, and QuPath​​.
+<br/><br/>
+</span>
+<span class="cil_14_bold_no_color">Technical details</span><br/>
+<span class="cil_13_no_color ">Scans of H&E-stained histology slides of biopsied tissue from endoscopy. Captured with MikroScan SL5 in SVS TIFF format with .tif file extension.
+<br/><br/></span>
+<!-------New link------------6-25-2017------->
+<a href="https://www.choc.org/research/" target="_blank">CHOC Research Institute - Children’s Hospital of Orange County</a>
+<br/><br/>
+<!---------------------------------->
+<div class="row">
+<div class="col-md-12">
+<span class="cil_title">View images by patient number</span>
+</div>
+    <div class="col-md-3"><a href="/images?simple_search=Search&k=P20483_patient_1" target="_blank" class="btn btn-info">Patient 1</a></div>   
+    <div class="col-md-3"><a href="/images?simple_search=Search&k=P20483_patient_2" target="_blank" class="btn btn-info">Patient 2</a></div>
+    <div class="col-md-3"><a href="/images?simple_search=Search&k=P20483_patient_3" target="_blank" class="btn btn-info">Patient 3</a></div>
+    <div class="col-md-3"><a href="/images?simple_search=Search&k=P20483_patient_4" target="_blank" class="btn btn-info">Patient 4</a></div>
+    <div class="col-md-12"><br/></div>
+    <div class="col-md-3"><a href="/images?simple_search=Search&k=P20483_patient_5" target="_blank" class="btn btn-info">Patient 5</a></div>
+    <div class="col-md-3"><a href="/images?simple_search=Search&k=P20483_patient_7" target="_blank" class="btn btn-info">Patient 7</a></div>
+    <div class="col-md-3"><a href="/images?simple_search=Search&k=P20483_patient_9" target="_blank" class="btn btn-info">Patient 9</a></div>
+    <div class="col-md-3"><a href="/images?simple_search=Search&k=P20483_patient_10" target="_blank" class="btn btn-info">Patient 10</a></div>
+    <div class="col-md-12"><br/></div>
+    <div class="col-md-3"><a href="/images?simple_search=Search&k=P20483_patient_11" target="_blank" class="btn btn-info">Patient 11</a></div>
+    <div class="col-md-3"><a href="/images?simple_search=Search&k=P20483_patient_13" target="_blank" class="btn btn-info">Patient 13</a></div>
+    <div class="col-md-3"><a href="/images?simple_search=Search&k=P20483_patient_14" target="_blank" class="btn btn-info">Patient 14</a></div>
+    <div class="col-md-3"><a href="/images?simple_search=Search&k=P20483_patient_17" target="_blank" class="btn btn-info">Patient 17</a></div>
+    <div class="col-md-12"><br/></div>
+    <div class="col-md-3"><a href="/images?simple_search=Search&k=P20483_patient_18" target="_blank" class="btn btn-info">Patient 18</a></div>
+    <div class="col-md-3"><a href="/images?simple_search=Search&k=P20483_patient_19" target="_blank" class="btn btn-info">Patient 19</a></div>
+    <div class="col-md-3"><a href="/images?simple_search=Search&k=P20483_patient_20" target="_blank" class="btn btn-info">Patient 20</a></div>
+    <div class="col-md-3"><a href="/images?simple_search=Search&k=P20483_patient_21" target="_blank" class="btn btn-info">Patient 21</a></div>
+    <div class="col-md-12"><br/></div>
+    <div class="col-md-3"><a href="/images?simple_search=Search&k=P20483_patient_22" target="_blank" class="btn btn-info">Patient 22</a></div>
+    <div class="col-md-3"><a href="/images?simple_search=Search&k=P20483_patient_23" target="_blank" class="btn btn-info">Patient 23</a></div>
+    <div class="col-md-3"></div>
+    <div class="col-md-3"></div>
+</div>
+<div class="row">
+    <div class="col-md-12"><br/></div>
+    <div class="col-md-12">
+    <span class="cil_title">Instructions for downloading the CHOC IBD dataset</span>
+    </div>
+    <div class="col-md-12">
+        <span class='cil_13_no_color'>1) To download the entire dataset, use the PHP file provided at <a href='https://github.com/CRBS/CIL_RS/wiki/Download_CHOC_dataset' target='_blank'>https://github.com/CRBS/CIL_RS/wiki/Download_CHOC_dataset</a></span>
+    </div>
+    <div class='col-md-12'>
+        <br/>
+        <span class='cil_13_no_color'>2) Each image file will be downloaded to a zip file. You need to extract the images from the zip files and save to a single directory.</span>
+    </div>
+    <div class='col-md-12'>
+        <br/>
+        <span class='cil_13_no_color'>3)The labels and data dictionary can be found in the excel file at <a href='https://cildata.crbs.ucsd.edu/media/images/55781/Pediatric_IBD_StructuredData.xlsx'>Pediatric_IBD_StructuredData.xlsx</a> </span>
+    </div>
+</div>
+        </div>
+    </div>
+</div>
+
+
